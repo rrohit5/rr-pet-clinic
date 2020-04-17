6a06b4de287cfcf24ed67c639129b2ed955e3f4c
rrohit5 <raghunathrohit24@gmail.com>
2020-04-17 19:52:50 +0530

    First Commit

---
+++ src/test/java/com/petclinic/RrPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.petclinic;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RrPetClinicApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
